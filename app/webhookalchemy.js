21036a02ebb8382dda3672de9a9cb857424b9429
cami le <125696442+EC-Art-Philosophy2023@users.noreply.github.com>
2023-06-16 05:21:52 +0000

    Update 3 files

---
+++ app/webhookalchemy.js
@@ -0,0 +1,23 @@
+{
+  "app": "Alchemy Mainnet",
+  "network": "MAINNET",
+  "webhookType": "DROPPED_TRANSACTION",
+  "hash": "0x7f65ae8e3d941be96ac1373152ada65316ac52e423f6c3848a37d5ac75c9c0c7",
+  "timestamp": "2020-06-08T22:12:57.126Z",
+  "fullTransaction": {
+    "hash": "0x7f65ae8e3d941be96ac1373152ada65316ac52e423f6c3848a37d5ac75c9c0c7",
+    "blockHash": null,
+    "blockNumber": null,
+    "from": "0x8a9d69aa686fa0f9bbdec21294f67d4d9cfb4a3e",
+    "gas": "0x5208",
+    "gasPrice": "0x165a0bc00",
+    "input": "0x",
+    "nonce": "0x2f",
+    "r": "0x7f65ae8e3d941be96ac1373152ada65316ac52e423f6c3848a37d5ac75c9c0c7",
+    "s": "0x25f5a3abf22f5b8ef6ed307a76e670f0c9fb4a71fab2621fce8b52da2ab8fe82",
+    "to": "0x3368d5aaDC7C4C0488eAFbAbb9ecaD0B10F53a34",
+    "transactionIndex": null,
+    "v": "0x1c",
+    "value": "0x1bc16d674ec80000"
+  }
+}
